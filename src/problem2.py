0a028cea4356885c0899798ae5e6ccbb0a6914da
josephhatfield211 <jhjoeman224@gmail.com>
2018-10-09 20:35:06 -0400

    done maybe

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 Exam 2, problem 2.
 
 Authors: Dave Fisher, David Mutchler, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Joey Hatfield.  October 2018.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 import time
@@ -109,8 +109,8 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem2a()
-    # run_test_problem2b()
+    run_test_problem2a()
+    run_test_problem2b()
 
 
 def run_test_problem2a():
@@ -196,11 +196,11 @@ def problem2a(triangle):
       :rtype: Triangle
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
-
+    trianglenew = Triangle(triangle.a*2,triangle.b*2,triangle.c*2)
+    return trianglenew
 def run_test_problem2b():
     """ Tests the   problem2b   function. """
 
@@ -358,7 +358,14 @@ def problem2b(triangles):
     #          Tests have been written for you (above).
     # IMPORTANT: See the HINT just before the DEF of this function.
     # -------------------------------------------------------------------------
-
+    sum = 0
+    if len(triangles) == 0:
+        return 0
+    else:
+        for k in range(len(triangles)):
+            triangle = triangles[k]
+            sum += triangle.get_area()
+    return sum
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
